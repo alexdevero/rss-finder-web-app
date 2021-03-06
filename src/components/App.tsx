d37741052e9cec9611d1fa0b6814ec9d1419dc99
Alex Devero <deveroalex@gmail.com>
2021-03-06 13:24:55 +0100

    Add app icon

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import logo from './images/logo.svg'
+import logo from './../images/rss-icon.svg'
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
