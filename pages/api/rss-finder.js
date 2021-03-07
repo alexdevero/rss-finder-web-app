4ee0c12ee71b9d189659082365108416a1e5ce66
Alex Devero <deveroalex@gmail.com>
2021-03-07 15:33:35 +0100

    Rewrite rss api endpoint

--- pages/api/rss-finder.js
+++ pages/api/rss-finder.js
@@ -2,16 +2,21 @@
 const rssFinder = require('rss-finder')
 
 export default async (req, res) => {
-  try {
-    const data = await rssFinder('http://www.nytimes.com')
-
-    res.status(200).json({
-      data: data
-    })
-  }
-  catch (err) {
+  if (req.body && req.body.websiteUrl) {
+    rssFinder(req.body.websiteUrl)
+      .then((data) => {
+        res.status(200).json({
+          feeds: data.feedUrls
+        })
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: err
+        })
+      })
+  } else {
     res.status(500).json({
-      data: err
+      message: err
     })
   }
 }
