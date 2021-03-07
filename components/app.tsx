e497239cba65b884710dccf0e4053f4776d45e0b
Alex Devero <deveroalex@gmail.com>
2021-03-07 15:55:30 +0100

    Implement axios for fetching rss feeds

--- components/app.tsx
+++ components/app.tsx
@@ -1,25 +1,49 @@
 import { useState } from 'react'
+import axios from 'axios'
+import sanitizeHtml from 'sanitize-html'
 
-interface feedUrlUI {
+interface FeedUI {
   title: string;
   url: string;
 }
 
 interface ResponseUI {
-    site: {
-        title: string;
-        favicon: string;
-        url: string
-    },
-    feedUrls: feedUrlUI[];
+  data: {
+    feeds: FeedUI[];
+  }
 }
 
 export function App() {
   const [feeds, setFeeds] = useState<string[]>([])
   const [websiteURL, setWebsiteURL] = useState('')
+  const [error, setError] = useState('')
 
-  const getFeedURs = () => {
+  const handleURLInput = (url: string) => {
+    let urlSanitized = sanitizeHtml(url)
+
+    if (urlSanitized.indexOf('http') === -1) {
+      urlSanitized = 'http://' + urlSanitized
+    }
+
+    setWebsiteURL(urlSanitized)
+    setError('')
+  }
+
+  const handleFeedURsFetching = () => {
     if (websiteURL.length > 0) {
+      axios.post('/api/rss-finder', { websiteUrl: websiteURL })
+        .then((response: ResponseUI) => {
+          if (response.data.feeds) {
+            setFeeds(response.data.feeds)
+            setError('')
+          } else if (response.body.message) {
+            setError(response.body.message)
+          }
+        })
+        .catch((error) => {
+          console.log('error: ', JSON.stringify(error, null, ' '))
+          setError('No RSS feeds found.')
+        })
     }
   }
 
@@ -27,9 +51,11 @@ export function App() {
     <div className="container pt-4">
       <div className="row justify-content-center">
         <div className="col-sm-10 col-md-7 col-lg-6 col-xl-5">
-          <img src="/images/rss-icon.svg" className="app-logo mx-auto d-block mb-4" alt="logo" width="100" />
+          <header>
+            <img src="/images/rss-icon.svg" className="app-logo mx-auto d-block mb-4" alt="logo" width="100" />
 
-          <h1 className="h3 text-center mb-4">RSS Finder</h1>
+            <h1 className="h3 text-center mb-4">RSS Finder</h1>
+          </header>
 
           <div className="input-group mb-4">
             <label htmlFor="urlInput" className="input-group-text">
@@ -41,23 +67,33 @@ export function App() {
               name="urlInput"
               id="urlInput"
               className="form-control"
-              placeholder="Enter website URL"
+              placeholder="Enter website URL (without http:// or https://)"
               aria-label="Website URL"
               aria-describedby="urlInput"
               defaultValue={websiteURL}
-              onChange={(event) => setWebsiteURL(event.target.value)}
+              onChange={(event) => handleURLInput(event.target.value)}
             />
           </div>
 
-          <div className="app-feeds-list">
-            <h2 className="h5">Found RSS feeds:</h2>
-
-            <ul>
-              {feeds.map((feedUrl) => {
-                return <li></li>
-              })}
-            </ul>
+          <div className="text-center">
+            <button className="btn btn-primary" onClick={handleFeedURsFetching}>Get feed URLs</button>
           </div>
+
+          {feeds.length > 0 && (
+            <div className="app-feeds-list mt-4">
+              <h2 className="h5 mb-2">Found RSS feeds:</h2>
+
+              <ul className="list-unstyled">
+                {feeds.map((feedUrl: FeedUI, index: number) => {
+                  return <li key={index}>{feedUrl.url}</li>
+                })}
+              </ul>
+            </div>
+          )}
+
+          {error.length > 0 && (
+            <div>{error}</div>
+          )}
         </div>
       </div>
     </div>
