f248c7d38e57b5a933e20316668b5f51a0d294ac
Alex Devero <deveroalex@gmail.com>
2021-03-09 16:11:26 +0100

    Change app header icon

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,6 +1,6 @@
 export const AppHeader = () => (
   <header>
-    <img src="/images/rss-icon.svg" className="app-logo mx-auto d-block mb-4" alt="logo" width="100" />
+    <img src="/images/rss-outlined-icon.svg" className="app-logo mx-auto d-block mb-4" alt="logo" width="100" />
 
     <h1 className="h3 text-center mb-4">RSS Finder</h1>
   </header>
